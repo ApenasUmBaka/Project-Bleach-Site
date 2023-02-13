8a7ff9113075bee8d1640a7a24fd4e8f787074a0
ApenasUmBaka <andresss2004@gmail.com>
2023-02-13 20:53:22 -0300

    Details Modal

--- src/js/index.js
+++ src/js/index.js
@@ -7,10 +7,20 @@ const close = document.querySelector(".modal__close");
 
 const video = document.getElementById("video");
 
+const buttonDetails = document.getElementById("btndetails__modal"); 
+
+const home = document.getElementById("menu");
+
+const modalDetails = document.querySelector(".details__modal")
+
 const linkVideo = video.src;
 
 function turnedModal() {
     modal.classList.toggle("--open");
+}
+
+function turnedDetails (){
+    modalDetails.classList.toggle("--open");
 }
 
 buttonTrailer.addEventListener("click", () => {
@@ -23,4 +33,14 @@ close.addEventListener("click", () => {
     video.setAttribute("src", "");
 });
 
+//Details Modal
 
+buttonDetails.addEventListener("click", () =>{
+    turnedDetails();
+    document.getElementById("main__container").style.visibility = "hidden";
+})
+
+home.addEventListener("click", () => {
+    modalDetails.classList.remove("--open");
+    document.getElementById("main__container").style.visibility = "visible";
+});
